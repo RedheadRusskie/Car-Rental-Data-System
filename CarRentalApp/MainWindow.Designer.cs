bcabe66a1d976e75a5643bce5ccc57452c4e77d3
Ivan <88277309+RedheadRusskie@users.noreply.github.com>
2021-12-28 21:36:20 +0100

    Add files via upload

---
+++ CarRentalApp/MainWindow.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace CarRentalApp
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.vehiclesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rentalRecordsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addRecordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewArchiveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LbEurorent = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vehiclesToolStripMenuItem,
+            this.rentalRecordsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(965, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // vehiclesToolStripMenuItem
+            // 
+            this.vehiclesToolStripMenuItem.Name = "vehiclesToolStripMenuItem";
+            this.vehiclesToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.vehiclesToolStripMenuItem.Text = "Vehicles";
+            this.vehiclesToolStripMenuItem.Click += new System.EventHandler(this.vehiclesToolStripMenuItem_Click);
+            // 
+            // rentalRecordsToolStripMenuItem
+            // 
+            this.rentalRecordsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addRecordToolStripMenuItem,
+            this.viewArchiveToolStripMenuItem});
+            this.rentalRecordsToolStripMenuItem.Name = "rentalRecordsToolStripMenuItem";
+            this.rentalRecordsToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
+            this.rentalRecordsToolStripMenuItem.Text = "Rental Records";
+            // 
+            // addRecordToolStripMenuItem
+            // 
+            this.addRecordToolStripMenuItem.Name = "addRecordToolStripMenuItem";
+            this.addRecordToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.addRecordToolStripMenuItem.Text = "Add record";
+            this.addRecordToolStripMenuItem.Click += new System.EventHandler(this.addRecordToolStripMenuItem_Click);
+            // 
+            // viewArchiveToolStripMenuItem
+            // 
+            this.viewArchiveToolStripMenuItem.Name = "viewArchiveToolStripMenuItem";
+            this.viewArchiveToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.viewArchiveToolStripMenuItem.Text = "View records";
+            this.viewArchiveToolStripMenuItem.Click += new System.EventHandler(this.viewArchiveToolStripMenuItem_Click);
+            // 
+            // LbEurorent
+            // 
+            this.LbEurorent.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.LbEurorent.AutoSize = true;
+            this.LbEurorent.BackColor = System.Drawing.Color.Transparent;
+            this.LbEurorent.Font = new System.Drawing.Font("Montserrat Medium", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LbEurorent.ForeColor = System.Drawing.Color.White;
+            this.LbEurorent.Location = new System.Drawing.Point(703, 472);
+            this.LbEurorent.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.LbEurorent.Name = "LbEurorent";
+            this.LbEurorent.Size = new System.Drawing.Size(248, 66);
+            this.LbEurorent.TabIndex = 16;
+            this.LbEurorent.Text = "eurorent";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(965, 547);
+            this.Controls.Add(this.LbEurorent);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainWindow";
+            this.Text = "eurorent EURODEX";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem vehiclesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rentalRecordsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addRecordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewArchiveToolStripMenuItem;
+        private System.Windows.Forms.Label LbEurorent;
+    }
+}
---
+++ CarRentalApp/MainWindow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CarRentalApp
+{
+    public partial class MainWindow : Form
+    {
+        /// <summary>
+        /// This is the landing window which first appears upon the program being launched. It is the MDI parent of all child elements and the primary means by which other windows can be navigated to. 
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region StripButtons
+
+        // Opens 'Add Record' window
+        private void addRecordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var addRecord = new MainForm();
+            addRecord.MdiParent = this;
+            addRecord.Show();
+        }
+
+        // Opens 'Vehicle list' window
+        private void vehiclesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var vehicleList = new VehicleListingManagement();
+            vehicleList.MdiParent = this;
+            vehicleList.Show();
+        }
+
+        // Opens 'Manage rental records' window
+        private void viewArchiveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var rentalRecords = new RentalRecordsManagement();
+            rentalRecords.MdiParent = this;
+            rentalRecords.Show();
+        }
+
+        #endregion
+    }
+}
